cea526d2137658ddc50f9bc6c43cd124325a2df8
Yunus Emre Unal <unaly21@itu.edu.tr>
2024-08-20 11:55:19 +0300

    INIT

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Halkbank on 20.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
