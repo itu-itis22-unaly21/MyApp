55995b22054f97906b8560e174d38f550c122e8c
itu-itis22-unaly21 <114349358+itu-itis22-unaly21@users.noreply.github.com>
2024-08-22 12:29:42 +0300

    Cell Fixes
    Fixes were made to make the cell exactly the same as given

--- MyApp/Scenes/Game/GameUITableViewCell.swift
+++ MyApp/Scenes/Game/GameUITableViewCell.swift
@@ -12,21 +12,33 @@ class GameUITableViewCell: UITableViewCell {
     
     let title: UILabel = {
         let label: UILabel = UILabel()
-        label.font = UIFont(name: "Roboto-Bold" , size: 20)
+        label.font = UIFont(name: "Roboto" , size: 20)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
         label.textColor = .black
         return label
     }()
+    let metaCriticLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = UIFont(name: "Roboto-Medium", size: 14)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .black
+        label.text = "metacritic: "
+        return label
+    }()
+
     
     let metaCritic: UILabel = {
         let label: UILabel = UILabel()
-        label.font = UIFont(name: "Roboto-Medium", size: 14)
+        label.font = UIFont(name: "Roboto", size: 18)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
         label.textColor = .red
         return label
     }()
     
     let categories: UILabel = {
         let label: UILabel = UILabel()
-        label.font = UIFont(name: "Roboto-Regular", size: 13)
+        label.font = UIFont(name: "Roboto", size: 13)
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         label.textColor = .gray
         return label
     }()
@@ -35,6 +47,7 @@ class GameUITableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(gameImageView)
         contentView.addSubview(title)
+        contentView.addSubview(metaCriticLabel)
         contentView.addSubview(metaCritic)
         contentView.addSubview(categories)
         
@@ -54,23 +67,31 @@ class GameUITableViewCell: UITableViewCell {
         }
         
         title.snp.makeConstraints { make in
-            make.left.equalTo(gameImageView.snp.right).offset(16)
-            make.top.equalTo(gameImageView.snp.top)
-            make.right.equalToSuperview().offset(-16)
+            make.width.equalTo(207)
             make.height.equalTo(27)
+            make.top.equalToSuperview().offset(16)
+            make.left.equalTo(gameImageView.snp.right).offset(16)
+        }
+        
+        metaCriticLabel.snp.makeConstraints { make in
+            make.width.equalTo(76)
+            make.height.equalTo(16)
+            make.top.equalToSuperview().offset(84)
+            make.left.equalTo(gameImageView.snp.right).offset(16)
         }
         
         metaCritic.snp.makeConstraints { make in
-            make.left.equalTo(title.snp.left)
-            make.top.equalTo(title.snp.bottom).offset(8)
+            make.width.equalTo(26)
             make.height.equalTo(16)
+            make.top.equalToSuperview().offset(84)
+            make.left.equalTo(metaCriticLabel.snp.right).offset(4)
         }
         
         categories.snp.makeConstraints { make in
-            make.left.equalTo(title.snp.left)
-            make.top.equalTo(metaCritic.snp.bottom).offset(8)
+            make.width.equalTo(207)
             make.height.equalTo(16)
-            make.right.equalToSuperview().offset(-16)
+            make.top.equalTo(metaCriticLabel.snp.bottom).offset(8)
+            make.left.equalTo(gameImageView.snp.right).offset(16)
         }
     }
 
